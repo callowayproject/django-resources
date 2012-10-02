11c8b86fda092c3307978df65bf3f90508f41009
Corey Oordt <coreyoordt@gmail.com>
2012-10-02 07:12:07 -0500

    Updating of the Slide and ExternalResource models
    * Some name changing from resource to supplycloset.
    * To better handle retrieving of actual vs. overridden attributes on Slide, the fields were renamed to FOO_ovr. __getattr__ now looks first at FOO_ovr then at content_object.FOO
    * Added auto discovery of resources

--- supplycloset/models.py
+++ supplycloset/models.py
@@ -8,7 +8,7 @@ from .settings import RESOURCE_TYPE_CHOICES, IMAGE_STORAGE, KEY_IMAGE_MODEL
 
 class ExternalResource(models.Model):
     """
-    An external resources
+    An external resource
     """
     resource_type = models.CharField(
         max_length=50,
@@ -19,13 +19,13 @@ class ExternalResource(models.Model):
         null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     key_image_custom = models.FileField(
-        upload_to='resources/resources',
+        upload_to='supplycloset/externalresources',
         storage=IMAGE_STORAGE(),
         blank=True, null=True)
     key_image_width = models.IntegerField(blank=True, null=True)
     key_image_height = models.IntegerField(blank=True, null=True)
     if KEY_IMAGE_MODEL:
-        key_image_related = models.ForeignKey(KEY_IMAGE_MODEL)
+        key_image_related = models.ForeignKey(KEY_IMAGE_MODEL, blank=True, null=True)
     credit = models.CharField(
         max_length=255,
         blank=True, null=True)
@@ -49,39 +49,75 @@ class Slide(models.Model):
     """
     An wrapper around another model
     """
+    overrides = ['resource_type', 'title', 'description', 'key_image',
+                 'credit', 'url', 'citation', 'notes', 'internal_ref']
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-    resource_type = models.CharField(
+    resource_type_ovr = models.CharField("resource type",
         max_length=50,
         choices=RESOURCE_TYPE_CHOICES,
         db_index=True)
-    title = models.CharField(
+    title_ovr = models.CharField("title",
         max_length=255,
         null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    description_ovr = models.TextField("description", null=True, blank=True)
     key_image_custom = models.FileField(
-        upload_to='resources/slides',
+        upload_to='supplycloset/slides',
         storage=IMAGE_STORAGE(),
         blank=True, null=True)
     key_image_width = models.IntegerField(blank=True, null=True)
     key_image_height = models.IntegerField(blank=True, null=True)
     if KEY_IMAGE_MODEL:
-        key_image_related = models.ForeignKey(KEY_IMAGE_MODEL)
-    credit = models.CharField(
+        key_image_related = models.ForeignKey(KEY_IMAGE_MODEL, blank=True, null=True)
+    credit_ovr = models.CharField("credit",
         max_length=255,
         blank=True, null=True)
-    url = models.CharField(
+    url_ovr = models.CharField("URL",
         max_length=255,
         blank=True, null=True)
-    citation = models.CharField(
+    citation_ovr = models.CharField("citation",
         max_length=255,
         blank=True, null=True)
-    notes = models.TextField(
+    notes_ovr = models.TextField("notes",
         blank=True, null=True)
-    internal_ref = models.CharField(
+    internal_ref_ovr = models.CharField("internal ref",
         max_length=255,
         blank=True, null=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Slide, self).__init__(*args, **kwargs)
+        self._resource = None
+
+    @property
+    def key_image_ovr(self):
+        """
+        Return the key_image_related or key_image_custom
+        """
+        if self.key_image_custom:
+            return self.key_image_custom
+        return getattr(self, 'key_image_related', None)
+
+    @property
+    def resource(self):
+        if self._resource is None and self.content_object:
+            self._resource = resource_list.get_for_instance(self.content_object)
+
+        return self._resource
+
+    def __getattr__(self, name):
+        """
+        Return either the overriddden value, or the original value, or raise
+        and AttributeError.
+        """
+        if name in self.overrides:
+            value = getattr(self, "%s_ovr" % name)
+            if value:
+                return value
+            if self.content_object:
+                return getattr(self.resource, name)
+        raise AttributeError("'Slide' object has no attribute '%s'" % name)
+
 
 resource_list.register(Slide, BaseResource)
 
@@ -116,8 +152,8 @@ class RelatedResource(models.Model):
 
     # ACTUAL RELATED OBJECT:
     object_type = models.ForeignKey(ContentType,
-        related_name="related_objects",
-        limit_choices_to=resource_list.content_types_lookup())
+        related_name="related_objects",)
+        # limit_choices_to=resource_list.content_types_lookup())
     object_id = models.IntegerField(db_index=True)
     object = GenericForeignKey(
         ct_field="object_type",
@@ -141,3 +177,6 @@ class RelatedResource(models.Model):
 
     def __unicode__(self):
         return u'%s related to %s' % (self.parent, self.object)
+
+from supplycloset import autodiscover
+autodiscover()
